96fafc91a6a87257be7967c5e383c4d17649b775
Tim Niklas Gruel <timniklas13@protonmail.com>
2024-07-25 16:48:05 +0200

    Initial Commit

---
+++ logym/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  logym
+//
+//  Created by Tim Niklas Gruel on 25.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ logym/logymApp.swift
@@ -0,0 +1,17 @@
+//
+//  logymApp.swift
+//  logym
+//
+//  Created by Tim Niklas Gruel on 25.07.24.
+//
+
+import SwiftUI
+
+@main
+struct logymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
